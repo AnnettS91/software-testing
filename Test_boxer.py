77c3511de4f6340da2f958d22153b95f82aa8415
Anna <subbota.anna.91@gmail.com>
2019-02-18 17:08:58 +0200

    new test

---
+++ Test_boxer.py
@@ -0,0 +1,62 @@
+import unittest
+import boxer
+
+class TestBoxer(unittest.TestCase):
+    def test_age(self):
+        self.assertEqual(boxer.Lucas.age, 33)
+
+    def test_weight(self):
+        self.assertEqual(boxer.Lucas.weight, 78)
+
+    def test_height(self):
+        self.assertEqual(boxer.Lucas.height, 180)
+
+    def test_age(self):
+        self.assertEqual(boxer.Neitan.age, 28)
+
+    def test_weight(self):
+        self.assertEqual(boxer.Neitan.weight, 72)
+
+    def test_height(self):
+        self.assertEqual(boxer.Neitan.height, 185)
+
+    def test_age(self):
+        self.assertEqual(boxer.Den.age, 35)
+
+    def test_weight(self):
+        self.assertEqual(boxer.Den.weight, 69)
+
+    def test_height(self):
+        self.assertEqual(boxer.Den.height, 182)
+
+    def test_age(self):
+        self.assertEqual(boxer.Lucas.age, 44)
+
+    def test_weight(self):
+        self.assertEqual(boxer.Lucas.weight, 90)
+
+    def test_height(self):
+        self.assertEqual(boxer.Lucas.height, 190)
+
+    def test_age(self):
+        self.assertEqual(boxer.Neitan.age, 32)
+
+    def test_weight(self):
+        self.assertEqual(boxer.Neitan.weight, 58)
+
+    def test_height(self):
+        self.assertEqual(boxer.Neitan.height, 170)
+
+    def test_age(self):
+        self.assertEqual(boxer.Den.age, 43)
+
+    def test_weight(self):
+        self.assertEqual(boxer.Den.weight, 98)
+
+
+    def test_height(self):
+
+        
+        self.assertEqual(boxer.Den.height, 196)
+
+       
---
+++ boxer.py
@@ -0,0 +1,9 @@
+class Boxer():
+    def __init__(self, age, weight, height):
+        self.age = age
+        self.weight = weight
+        self.height = height
+
+Lucas  = Boxer(age=33, weight=78, height=180)
+Neitan = Boxer(age=28, weight=72, height=185)
+Den = Boxer(age=35, weight=69, height=182)
